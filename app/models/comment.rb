50a82ddb2e33ab666559b054ceb4d4e48040ebbd
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-04 06:54:34 +0200

    Added linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: "User", foreign_key: "author_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User", foreign_key: "author_id"
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes
+  has_many :comments
 end
