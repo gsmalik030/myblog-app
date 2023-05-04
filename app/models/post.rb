16245aa6604302d71eb4b064b15ba7e3430db64a
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-04 15:16:56 +0200

    Fixed rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
-  
+
   after_save :update_posts_counter
 
   def return_recent_comments
