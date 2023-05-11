fde996b85e79fae7c44fed2a774b627c10309d42
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-11 11:03:53 +0200

    Added forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index create show]
   def index
     set_user
     @posts = @user.posts
@@ -8,6 +9,21 @@ class PostsController < ApplicationController
     set_post
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
   private
 
   def set_post
@@ -16,5 +32,8 @@ class PostsController < ApplicationController
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
