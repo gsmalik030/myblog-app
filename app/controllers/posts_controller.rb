a6f85e22cd6511fc68c16673d180ced6f2876abb
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-16 10:40:22 +0200

    Fixed N + 1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.includes(:author).find(params[:id])
   end
 
   def set_user
