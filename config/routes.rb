9fc3f1780489272d5fc02bf88bd4ff855f582470
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-17 18:07:38 +0200

    Added signout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   root 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
@@ -8,3 +13,4 @@ Rails.application.routes.draw do
     end
   end
 end
+
